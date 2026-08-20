b96fe220b26be15669ea2b0c29a79823b1d36d94
muhammed-ahmed24 <muhammedahmed2402@gmail.com>
2026-08-20 19:33:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Day1CSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
